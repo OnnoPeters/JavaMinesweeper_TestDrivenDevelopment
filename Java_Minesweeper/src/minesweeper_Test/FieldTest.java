3723a829eea292c0dc2091cc2f087785ea9cc064
OnnoPeters <onno.peters@dinkla.net>
2021-02-07 17:27:35 +0100

    Encapsulated fields and added new test to check for the total number of mines and to validate displayed number

--- Java_Minesweeper/src/minesweeper_Test/FieldTest.java
+++ Java_Minesweeper/src/minesweeper_Test/FieldTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
+import java.util.Random;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -19,7 +20,7 @@ public class FieldTest
     {
         field = new Field(size);
 
-        assertSame(field.cellsVisible.length, field.size);
+        assertSame(field.cellsVisible.length, field.getSize());
         for (int i = 0; i < field.cellsVisible.length; i++)
         {
             assertSame(field.cellsVisible.length, field.cellsVisible[i].length);
@@ -36,6 +37,43 @@ public class FieldTest
     {
         field = new Field(size);
 
-        assertTrue(field.size == field.max || field.size == field.min);
+        assertTrue(field.getSize() == field.getMax() || field.getSize() == field.getMin());
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {0,1,6,21,45,53,74,123,5654,4323443,-543})
+    public void testMines(int mines)
+    {
+        Random rand = new Random();
+        testInvalidValues(rand.nextInt(field.getMax() - field.getMin()) + field.getMin());
+
+        field.setMines(mines);
+
+        int countTotalMines = 0;
+        for (int i = 0; i < field.cellsVisible.length; i++) {
+            for (int j = 0; j < field.cellsVisible[i].length; j++) {
+                if(field.getNumberAtLocation(i,j) == -1)
+                {
+                    countTotalMines++;
+                }
+                else
+                {
+                   int countNeighbouringMines = 0;
+                   for(int k = i-1; k <= i+1; k++)
+                   {
+                       for(int m = j-1; m <= j+1; m++)
+                       {
+                           if(k < field.cellsVisible.length && m < field.cellsVisible[i].length && field.getNumberAtLocation(k,m) == -1)
+                           {
+                               countNeighbouringMines++;
+                           }
+                       }
+                   }
+                   assertTrue(field.getNumberAtLocation(i,j) == countNeighbouringMines);
+                }
+
+            }
+        }
+        assertTrue(countTotalMines > 0 && countTotalMines < field.getSize() * field.getSize());
     }
 }
