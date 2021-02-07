76e57f288214fa8d5d0259421ea6295f4d1193fe
OnnoPeters <onno.peters@dinkla.net>
2021-02-07 15:17:11 +0100

    Added Field class that passes all written tests

--- Java_Minesweeper/src/minesweeper_Code/Field.java
+++ Java_Minesweeper/src/minesweeper_Code/Field.java
@@ -2,4 +2,39 @@ package minesweeper_Code;
 
 public class Field
 {
+
+    public int max;
+    public int min;
+    public int size;
+    public String[][] cellsVisible;
+
+    public Field(int size)
+    {
+        this();
+        if(size > this.max)
+        {
+            size = this.max;
+        }
+        else if(size < this.min)
+        {
+            size = this.min;
+        }
+        this.size = size;
+
+        this.cellsVisible = new String[size][size];
+        for(int i = 0; i < this.size; i++)
+        {
+            for(int j = 0; j < this.size; j++)
+            {
+                this.cellsVisible[i][j] = "*";
+            }
+        }
+    }
+
+    private Field()
+    {
+        this.max = 100;
+        this.min = 10;
+    }
+
 }
