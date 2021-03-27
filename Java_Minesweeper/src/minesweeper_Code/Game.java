d87062cd378798838c0843c07505c494253b9364
OnnoPeters <onno.peters@dinkla.net>
2021-03-27 19:02:53 +0100

    Added functionality to both win and lose

--- Java_Minesweeper/src/minesweeper_Code/Game.java
+++ Java_Minesweeper/src/minesweeper_Code/Game.java
@@ -3,38 +3,78 @@ package minesweeper_Code;
 public class Game
 {
     public Field field;
+    private boolean winningStatus;
+    private boolean endingStatus;
 
     public Game(int size, int mines)
     {
         this.field = new Field(size);
         this.field.setMines(mines);
+        setEndingStatus(false);
+        setWinningStatus(false);
+    }
+
+    public boolean getEndingStatus()
+    {
+        return endingStatus;
+    }
+
+    public void setEndingStatus(boolean endingStatus)
+    {
+        this.endingStatus = endingStatus;
+    }
+
+    public boolean getWinningStatus()
+    {
+        return winningStatus;
+    }
+
+    public void setWinningStatus(boolean winningStatus)
+    {
+        this.winningStatus = winningStatus;
     }
 
     public boolean action(int loc1, int loc2)
     {
-        if(loc1 < field.getSize() && loc2 < field.getSize() && loc1 >= 0 && loc2 >= 0)
+        if(!getEndingStatus())
         {
-            this.field.printVisibleField();
-            this.field.printHiddenField();
-            int numberAtLoc = this.field.getNumberAtLocation(loc1, loc2);
-            if(numberAtLoc == -1)
+            if (loc1 < field.getSize() && loc2 < field.getSize() && loc1 >= 0 && loc2 >= 0)
             {
-                this.field.setVisibleCellAtLocation(loc1, loc2, "X");
+                this.field.printVisibleField();
+                this.field.printHiddenField();
+                int numberAtLoc = this.field.getNumberAtLocation(loc1, loc2);
+                if (numberAtLoc == -1)
+                {
+                    this.field.setVisibleCellAtLocation(loc1, loc2, "X");
+                    endGameUnsuccessfully();
+                }
+                else
+                {
+                    this.field.setVisibleCellAtLocation(loc1, loc2, String.valueOf(numberAtLoc));
+                }
+                this.field.printVisibleField();
+                this.field.printHiddenField();
+                return true;
             }
             else
             {
-                this.field.setVisibleCellAtLocation(loc1, loc2, String.valueOf(numberAtLoc));
+                return false;
             }
-            this.field.printVisibleField();
-            this.field.printHiddenField();
-            return true;
         }
         else
         {
             return false;
         }
-
-
     }
 
+    public void endGameSuccessfully()
+    {
+        setEndingStatus(true);
+        setWinningStatus(true);
+    }
+
+    public void endGameUnsuccessfully()
+    {
+        setEndingStatus(true);
+    }
 }
