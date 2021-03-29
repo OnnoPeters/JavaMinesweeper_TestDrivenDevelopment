d44c6c78fdadcb271d9e9a264c373ab370aef69f
OnnoPeters <onno.peters@dinkla.net>
2021-03-29 22:48:16 +0200

    Input should also verify that input is the right type

--- Java_Minesweeper/src/minesweeper_Test/InputTest.java
+++ Java_Minesweeper/src/minesweeper_Test/InputTest.java
@@ -14,34 +14,45 @@ import static org.junit.jupiter.api.Assertions.*;
 public class InputTest
 {
     private Input input;
-    private int rand_size;
-    private int rand_mines;
+    private String rand_size;
+    private String rand_mines;
 
     @BeforeEach
     public void init()
     {
         input = new Input();
         Random rand = new Random();
-        rand_size = rand.nextInt(90) + 10;
-        rand_mines = rand.nextInt(rand_size * rand_size - 2) + 1;
+        rand_size = String.valueOf(rand.nextInt(90) + 10);
+        rand_mines = String.valueOf(rand.nextInt(Integer.parseInt(rand_size) * Integer.parseInt(rand_size) - 2) + 1);
     }
 
     @ParameterizedTest
-    @CsvSource({"32,66", "5,2", "1,3", "123, 21", "77, 321", "0,0", "65,57", "-1,0", "5, -5", "-4, 129", "3,20"})
-    public void testValidateInput(int size, int mines)
+    @CsvSource({"32,66", "5,2", "1,3", "123, 21", "77, 321", "0,0", "65,57", "-1,0", "5, -5", "-4, 129", "3,20", "3F,6", "hgfdgdf,hgfddh", "4s, 5s", "10,5g"})
+    public void testValidateInput(String size, String mines)
     {
         input.enterSize(size);
         input.enterMines(mines);
         boolean validInputs = input.validateInput();
         if (validInputs)
         {
-            assertTrue(input.getSize() <= 100);
-            assertTrue(input.getMines() < input.getSize() * input.getSize());
-            assertTrue(input.getMines() > 0 && input.getSize() > 0);
+            int intSize = Integer.parseInt(input.getSize());
+            int intMines = Integer.parseInt(input.getMines());
+            assertTrue(intSize <= 100);
+            assertTrue(intMines < intSize * intSize);
+            assertTrue(intMines > 0 && intSize > 0);
         }
         else
         {
-            assertTrue(input.getSize() > 100 || input.getMines() >= input.getSize() * input.getSize() || input.getMines() <= 0 || input.getSize() <= 0);
+            if(input.convertToIntPossible())
+            {
+                int intSize = Integer.parseInt(input.getSize());
+                int intMines = Integer.parseInt(input.getMines());
+                assertTrue(intSize > 100 || intMines >= intSize * intSize || intMines <= 0 || intSize <= 0);
+            }
+            else
+            {
+                assertFalse(input.convertToIntPossible());
+            }
         }
     }
 
@@ -60,6 +71,5 @@ public class InputTest
             Game game = input.startGame();
             assertNull(game);
         }
-
     }
 }
