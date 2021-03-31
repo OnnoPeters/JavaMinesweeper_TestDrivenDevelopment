d235c2b65602ffee9ba9629b5fbce6d920ea7c43
OnnoPeters <onno.peters@dinkla.net>
2021-03-31 19:20:34 +0200

    Main creates UI

--- Java_Minesweeper/src/minesweeper_Code/Main.java
+++ Java_Minesweeper/src/minesweeper_Code/Main.java
@@ -1,9 +1,11 @@
 package minesweeper_Code;
 
+import minesweeper_UI.GameMenuUI;
+
 public class Main
 {
     public static void main(String[] args)
     {
-
+        GameMenuUI ui = new GameMenuUI();
     }
 }
